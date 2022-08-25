97bdfbdaf179371bcde5bbc27c84566c20c98251
vanparfait <nguemechiaparfait@gmail.com>
2022-08-25 18:20:50 +0100

    les proprietes en react

---
+++ src/components/AnimalCard/AnimalCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./AnimalCard.css";
+import PropTypes from "prop-types";
+
+const AnimalCard = ({
+  name,
+  additional,
+  diet,
+  scientificName,
+  showAdditional,
+  size,
+}) => {
+  //   const name = props.name;
+
+  return (
+    <div className="animal-wrapper">
+      <h2>{name}</h2>
+      <h3>{scientificName}</h3>
+      <h4>{size}kg</h4>
+      <div>{diet.join(", ")}.</div>
+      <button onClick={() => showAdditional(additional)}>More plus</button>
+    </div>
+  );
+};
+
+AnimalCard.propTypes = {
+  additional: PropTypes.shape({
+    link: PropTypes.string,
+    notes: PropTypes.string,
+  }),
+  diet: PropTypes.arrayOf(PropTypes.string).isRequired,
+  name: PropTypes.string.isRequired,
+  scientificName: PropTypes.string.isRequired,
+  showAdditional: PropTypes.func.isRequired,
+  size: PropTypes.number.isRequired,
+};
+
+AnimalCard.defaultProps = {
+  additional: {
+    notes: "No Additional Information",
+  },
+};
+export default AnimalCard;
